f8b3b9f8b0c62c3ed245396a504e9f0be68f73f4
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-06 16:45:17 -0800

    Code reuse with interfaces

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -3,12 +3,29 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
+}
+
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const drinks = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 // without the interface
 const printVehicle = (vehicle: {
@@ -27,5 +44,13 @@ const printVehicle2 = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Name: ${vehicle.year}`);
   console.log(`Name: ${vehicle.broken}`);
+  console.log(vehicle.summary());
+};
+
+// Rehusable code using generic looking functions, we can use the reportable interface in both objects
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary);
 };
 printVehicle2(oldCivic);
+printSummary(oldCivic);
+printSummary(drinks);
