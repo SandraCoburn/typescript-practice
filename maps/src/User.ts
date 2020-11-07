73f372341fabb6c5314934d36b6e0d411d779d4c
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-07 12:07:36 -0800

    Using Type Definition Files

---
+++ maps/src/User.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+
+// initialize inside the constructor
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()), // use parseFloat to turn string into a number
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
