3763035542e30bc71ada9ec1ab83d2b7caa42089
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-09 11:24:44 -0800

    One possible solution to have a reusable marker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -10,23 +10,27 @@ export class CustomMap {
       center: { lat: 0, lng: 0 },
     });
   }
-  //instance of user and company * bad code
-  addUserMarker(user: User): void {
+
+  //One solution to have a reusable marker
+  addMarker(mappable: User | Company): void {
+    //TS will look only at the properties both share: location
+
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  }
+  //instance of user and company * bad code because it repeats on user and company markers
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng,
+  //     },
+  //   });
+  // }
 }
