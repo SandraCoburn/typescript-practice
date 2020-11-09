0ecf5423e8857e68e07b29f0a5db5f476a52bbdf
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-09 11:10:32 -0800

    Added a user's marker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map; // so no one else can get a hold of this out of this class
   constructor(divId: string) {
@@ -6,4 +9,15 @@ export class CustomMap {
       center: { lat: 0, lng: 0 },
     });
   }
+  //instance of user and company * bad code
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+  addCompanyMarker(company: Company): void {}
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './User';
-import { Company } from './Company';
-import { googlemaps } from 'googlemaps';
+// import { Company } from './Company';
+// import { googlemaps } from 'googlemaps';
 import { CustomMap } from './CustomMap';
 
-new CustomMap('map');
+const user = new User();
+// pass the class name as an argument so if you have more instances of cCustomMap you'll use other classnames
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
