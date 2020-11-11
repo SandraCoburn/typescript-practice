ba02f07d372dafc3a2463b6c7e42954a81f8b49c
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-11 12:03:42 -0800

    Separated swapping and comparison to make Sort more generic

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -1,25 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    const length = this.collection.length;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // If collection is an array of number, using Type Guards
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-        // If collection is a string, do this logic
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
-const sorter = new Sorter([10, 2, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
