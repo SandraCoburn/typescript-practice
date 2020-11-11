c4b6ff93e08f897c4ab57b6d31185436abb23e1d
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-11 14:04:23 -0800

    Added class to sort strings - CharactersCollection

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xanabbe');
+const sorterStr = new Sorter(charactersCollection);
+sorterStr.sort();
+console.log(charactersCollection.data);
