65dd59d2ac1aa4abe0f5cee38abeb86b1318e26f
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-11 13:49:10 -0800

    Added interface to make sorting generic

--- Sort/src/Sorter.ts
+++ Sort/src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from './NumbersCollection';
-
+// interface that will show what you need to have to be eligible for sorting
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const length = this.collection.length;
