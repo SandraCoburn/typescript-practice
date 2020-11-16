88c0bb30135fe8ad6dd081b656d4ac4a2b2b8e42
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-16 15:55:53 -0800

    TypeAssertions

--- stats/build/index.js
+++ stats/build/index.js
@@ -2,24 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //import file system from node modules. To stop the error, load type definition file
 var CsvFileReader_1 = require("./CsvFileReader");
+var MatchResult_1 = require("./MatchResult");
 //Parse the data
 var reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
 //Analyze the date by counting how many times there are wins for man United
-//enum = enumeration
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
 var manUnitedWins = 0;
 for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
     var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,12 @@
 //import file system from node modules. To stop the error, load type definition file
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 //Parse the data
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
 //Analyze the date by counting how many times there are wins for man United
-//enum = enumeration
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
+
 let manUnitedWins = 0;
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
