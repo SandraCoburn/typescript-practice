7c794188b25cc9958b2bb4b159e8b9b1b0dab06f
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-16 13:14:42 -0800

    Using Enums

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -9,12 +9,19 @@ const matches = fs
   .map((row: string): string[] => {
     return row.split(',');
   });
+
 //Analyze the date by counting how many times there are wins for man United
+//enum = enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
 let manUnitedWins = 0;
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
