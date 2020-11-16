36ffc52a12fec02da5c2c6164bef751d43672edf
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-16 12:52:50 -0800

    Type definition files for Node, reading from csv files

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hi there!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//import file system from node modules. To stop the error, load type definition file
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8',
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+console.log(matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hi there!');
+//import file system from node modules. To stop the error, load type definition file
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+console.log(matches);
