bc13f1612c0f6490d95b44bf8fb7361552944688
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-16 13:04:25 -0800

    Analyzing data to check for how many wins a team had

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,6 @@
 //import file system from node modules. To stop the error, load type definition file
 import fs from 'fs';
-
+//Parse the data
 const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8',
@@ -9,4 +9,13 @@ const matches = fs
   .map((row: string): string[] => {
     return row.split(',');
   });
-console.log(matches);
+//Analyze the date by counting how many times there are wins for man United
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United won ${manUnitedWins} games`);
