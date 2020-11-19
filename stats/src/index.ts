c2e22ea31e0da86ce0137d46b972036e778e7390
Sandra Coburn <sandra_coburn@hotmail.com>
2020-11-18 18:47:04 -0800

    Last refactoring of the code using interfaces

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,10 @@
 //import file system from node modules. To stop the error, load type definition file
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/Console.Reports';
+import { Summary } from './Summary';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+
 //Create an object that satisfies the "DataReader" interface
 const csvFileReader = new CsvFileReader('football.csv');
 
@@ -11,13 +14,9 @@ const matchReader = new MatchReader(csvFileReader);
 //Parse the data
 matchReader.load();
 
-//Analyze the date by counting how many times there are wins for man United
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+//Analyze data
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+summary.buildAndPrintReport(matchReader.matches);
